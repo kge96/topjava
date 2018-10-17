b2abe28822740a8871c58b5e5334d46159d5fc03
gkuz <kuznetsovge@yandex.ru>
2018-10-17 08:53:23 +0500

    hw2_add_meal_repository_impl

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,22 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface MealService {
+
+    Meal create(Meal meal);
+
+    void delete(int id) throws NotFoundException;
+
+    Meal get(int id) throws NotFoundException;
+
+    void update(Meal meal);
+
+    List<Meal> getAll();
+
+    List<Meal> getBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,10 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 public class MealServiceImpl implements MealService {
-
+    @Autowired
     private MealRepository repository;
 
 }

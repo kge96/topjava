6a281cdb341b42b9dc667f84cc6fe6e9052556a2
gkuz <kuznetsovge@yandex.ru>
2018-12-20 21:29:10 +0500

    10_14_read_write_access

--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -1,14 +1,11 @@
 package ru.javawebinar.topjava.web.json;
 
-import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.View;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static ru.javawebinar.topjava.MealTestData.*;
 
 class JsonUtilTest {
@@ -27,13 +24,5 @@ class JsonUtilTest {
         System.out.println(json);
         List<Meal> meals = JsonUtil.readValues(json, Meal.class);
         assertMatch(meals, MEALS);
-    }
-
-    @Test
-    public void testWriteWithView() throws Exception {
-        ObjectWriter uiWriter = JacksonObjectMapper.getMapper().writerWithView(View.JsonUI.class);
-        String json = JsonUtil.writeValue(ADMIN_MEAL1, uiWriter);
-        System.out.println(json);
-        assertThat(json, containsString("dateTimeUI"));
     }
 }
